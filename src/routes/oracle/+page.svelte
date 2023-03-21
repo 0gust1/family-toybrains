6e895a1700fcb7898da60ba58e66aa0b51f9cd3f
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-03-21 01:21:51 +0100

    feat: styling - robot ! - messages styling - error display

--- src/routes/oracle/+page.svelte
+++ src/routes/oracle/+page.svelte
@@ -5,12 +5,17 @@
 	import CopyToClipboardButton from '$lib/components/CopyToClipboardButton.svelte';
 	import Spinner from '$lib/components/Spinner.svelte';
 	import { marked } from 'marked';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 	export let form;
 	let debug = true;
 	let model = data.model;
 	let isSubmitting = false;
+
+	onMount(() => {
+		window.scrollTo(0, document.body.scrollHeight);
+	});
 </script>
 
 <h1 class=" text-3xl">L'oracle bête et cultivé d'Internet, en mode kawai</h1>
@@ -27,23 +32,18 @@
 	</pre>
 </div> -->
 
-<div>
-	{#if form && form.error}
-		<div class="m-4 p-4 border-2 border-red-500">
-			{form.error}
-		</div>
-	{/if}
-</div>
-
 <div class="m-4 p-4">
-	<div>
+	<div class="w-3/5 mx-auto">
 		{#each data.messages as msg}
 			<div
 				class:role-assistant={msg.message.role === 'assistant'}
 				class:role-user={msg.message.role === 'user'}
 				class="message"
 			>
-				<div class="flex">
+				<div class="flex relative">
+					{#if msg.message.role === 'assistant'}
+						<img src="/robot1.svg" class=" w-16 h-16 absolute -top-4 -left-24 " />
+					{/if}
 					<article class="content prose">
 						{@html marked.parse(msg.message.content)}
 						<!-- {msg.message.content.trimStart()} -->
@@ -66,7 +66,8 @@
 		{/if}
 	</div>
 	<form
-		class="w-full"
+		id="prompt-form"
+		class="w-3/5 mx-auto"
 		method="POST"
 		use:enhance={({ form, data, action, cancel, submitter }) => {
 			// `form` is the `<form>` element
@@ -83,8 +84,8 @@
 			};
 		}}
 	>
-		<div class="flex gap-2 w-full mb-6">
-			<label class="flex w-1/2">
+		<div class="flex gap-2 w-full items-center mb-6">
+			<label class="flex w-full">
 				<textarea
 					name="message"
 					required
@@ -100,6 +101,7 @@
 			{:else}
 				<Spinner />
 			{/if}
+
 			{#if form && form.lastUsage}
 				<div class="text-xs text-right grow">
 					<strong>tokens:</strong> prompt: {form?.lastUsage?.prompt_tokens} | completion: {form
@@ -107,6 +109,22 @@
 				</div>
 			{/if}
 		</div>
+
+		{#if form && form.error}
+			<div class="m-4 p-2 flex items-end text-xs">
+				<img src="/robot1.svg" alt="" class="w-14 h-14" />
+				<div class="px-2">
+					<p class="text-sm font-medium text-red-700">{form.message}</p>
+					<div class="bg-orange-100 p-2 border-2 border-red-400 rounded-md">
+						<p>({form.error.error.code})</p>
+						<p>
+							{form.error.error.message}
+						</p>
+					</div>
+					<!-- {JSON.stringify(form.error)} -->
+				</div>
+			</div>
+		{/if}
 		{#if debug}
 			<div class="debug">
 				<label>
@@ -124,17 +142,32 @@
 
 <style lang="postcss">
 	.message {
-		@apply my-2 px-4 py-6 border-2 border-opacity-50 border-slate-200 rounded;
+		@apply my-2 px-4 py-6 border-2 border-opacity-60 border-slate-200 rounded;
 	}
 	.message .content {
 		/* @apply whitespace-pre-line; */
 		@apply prose prose-slate prose-sm;
 	}
 	.role-assistant {
-		@apply mr-9 pl-6 border-blue-200 border-opacity-20 bg-blue-100 text-slate-800 bg-opacity-50 rounded-r-2xl;
+		@apply relative mr-9 pl-6 border-blue-200 border-opacity-20 bg-blue-100 text-slate-800 bg-opacity-70 rounded-r-2xl shadow-md;
 	}
+
+	/* 	.role-assistant:before {
+		content: '';
+		position: absolute;
+		width: 0;
+		height: 0;
+		bottom: 100%;
+		@apply -left-6 top-6; 
+		border: 0.9rem solid transparent;
+		border-top: none;
+
+		@apply border-b-blue-100 border-opacity-60;
+		transform: rotate(-90deg) translateY(0.1rem);
+		filter: drop-shadow(0 -0.0625rem 0.0625rem rgba(0, 0, 0, 0.1));
+	} */
 	.role-user {
-		@apply ml-9 border-green-200 border-opacity-20 bg-green-100 text-slate-800 bg-opacity-50 rounded-l-2xl;
+		@apply ml-9 border-green-200 border-opacity-20 bg-green-100 text-slate-800 bg-opacity-50 rounded-l-2xl shadow-md;
 	}
 	.debug {
 		@apply mt-2 text-xs bg-yellow-200 bg-opacity-30 p-4 rounded-lg;
