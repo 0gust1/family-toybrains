1d54c1162f6fd769886d097a9fd35a71c1081c89
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-03-23 15:13:21 +0100

    feat: disconnect button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script>
 	import '../app.postcss';
+
+	const disconnect = () => {
+		window.location.href = '/logout';
+	};
 </script>
 
-<div class=" bg-pink-100 w-full flex justify-end px-4">
-	<button type="button">Se déconnecter</button>
+<div class=" bg-pink-100 w-full flex justify-end px-4 py-1">
+	<button type="button" class="text-xs p-2 rounded border border-purple-500" on:click={disconnect}
+		>Se déconnecter</button
+	>
 </div>
 
 <div class=" bg-pink-200 h-full min-h-screen">
