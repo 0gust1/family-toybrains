1c838001653f4850abecd6c4db7e79510a0c6158
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-03-25 14:46:33 +0100

    fix: refresh conversation list when one was created

--- src/routes/oracle/+layout.svelte
+++ src/routes/oracle/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Sidebar from './Sidebar.svelte';
 	export let data;
 	let conversations = data.conversations ?? [];
 </script>
 
-<Sidebar {conversations}>
+<Sidebar conversations={$page.data.conversations}>
 	<main class="flex-1">
 		<div class="">
 			<slot />
